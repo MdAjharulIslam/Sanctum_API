4dea99ae855cc5ca0d5a86942000b41f6ecaad6c
ajharul <mdajharulislam327@gmail.com>
2026-05-02 00:02:41 +0600

    API

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function signup(Request $request){
+
+    }
+
+    public function login(Request $request){
+
+    }
+
+    public function logout(Request $request){
+
+    }
+    
+}
